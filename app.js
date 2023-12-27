379400092940a986e66f846b9791e89bd298b6da
anabelen19891994 <ana.b.coronel@outlook.com>
2023-12-27 15:30:25 -0300

    agregado de libreria

--- app.js
+++ app.js
@@ -1,3 +1,154 @@
+// const btnCart = document.querySelector('.container-cart-icon');
+// const containerCartProducts = document.querySelector('.container-cart-products');
+
+// btnCart.addEventListener('click', () => {
+//     containerCartProducts.classList.toggle('hidden-cart');
+// });
+
+// const cartInfo = document.querySelector('.cart-product');
+// const rowProduct = document.querySelector('.row-product');
+
+// const productsList = document.querySelector('.container-items');
+
+// let allProducts = [];
+
+// const valorTotal = document.querySelector('.total-pagar');
+
+// const countProducts = document.querySelector('#contador-productos');
+
+// const cartEmpty = document.querySelector('.cart-empty');
+// const cartTotal = document.querySelector('.cart-total');
+
+// productsList.addEventListener('click', async (e) => {
+//     if (e.target.classList.contains('btn-add-cart')) {
+//         const product = e.target.parentElement;
+
+//         const infoProduct = {
+//             quantity: 1,
+//             title: product.querySelector('h2').textContent,
+//             price: product.querySelector('p').textContent,
+//         };
+
+//         const exists = allProducts.findIndex((prod) => prod.title === infoProduct.title);
+
+//         if (exists !== -1) {
+//             allProducts[exists].quantity++;
+//         } else {
+//             allProducts.push(infoProduct);
+//         }
+
+//         showHTML();
+
+//         try {
+//             await saveProductsLocally();
+//         } catch (error) {
+//             console.error('Error saving products locally:', error);
+//         }
+//     }
+// });
+
+// rowProduct.addEventListener('click', async (e) => {
+//     if (e.target.classList.contains('icon-close')) {
+//         const product = e.target.parentElement;
+//         const title = product.querySelector('p').textContent;
+
+//         allProducts = allProducts.filter((prod) => prod.title !== title);
+
+//         showHTML();
+
+//         try {
+//             await saveProductsLocally();
+//         } catch (error) {
+//             console.error('Error saving products locally:', error);
+//         }
+//     }
+// });
+
+// const showHTML = () => {
+//     if (!allProducts.length) {
+//         cartEmpty.classList.remove('hidden');
+//         rowProduct.classList.add('hidden');
+//         cartTotal.classList.add('hidden');
+//     } else {
+//         cartEmpty.classList.add('hidden');
+//         rowProduct.classList.remove('hidden');
+//         cartTotal.classList.remove('hidden');
+//     }
+
+//     rowProduct.innerHTML = '';
+
+//     let total = 0;
+//     let totalOfProducts = 0;
+
+//     allProducts.forEach((product) => {
+//         const containerProduct = document.createElement('div');
+//         containerProduct.classList.add('cart-product');
+
+//         containerProduct.innerHTML = `
+//             <div class="info-cart-product">
+//                 <span class="cantidad-producto-carrito">${product.quantity}</span>
+//                 <p class="titulo-producto-carrito">${product.title}</p>
+//                 <span class="precio-producto-carrito">${product.price}</span>
+//             </div>
+//             <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 stroke-width="1.5"
+//                 stroke="currentColor"
+//                 class="icon-close"
+//             >
+//                 <path
+//                     stroke-linecap="round"
+//                     stroke-linejoin="round"
+//                     d="M6 18L18 6M6 6l12 12"
+//                 />
+//             </svg>
+//         `;
+
+//         rowProduct.append(containerProduct);
+
+//         total += parseInt(product.quantity * product.price.slice(1));
+//         totalOfProducts += product.quantity;
+//     });
+
+//     valorTotal.innerText = `$${total}`;
+//     countProducts.innerText = totalOfProducts;
+// };
+
+// const fetchData = async () => {
+//     try {
+//         const response = await fetch('URL_DE_TU_API_O_DATOS');
+//         const data = await response.json();
+//         // Procesar los datos recibidos si es necesario
+//         console.log('Datos obtenidos:', data);
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//     }
+// };
+
+// const saveProductsLocally = async () => {
+//     try {
+//         localStorage.setItem('products', JSON.stringify(allProducts));
+//     } catch (error) {
+//         throw new Error('Error saving products to local storage');
+//     }
+// };
+
+// window.addEventListener('load', async () => {
+//     try {
+//         const storedProducts = JSON.parse(localStorage.getItem('products'));
+//         if (storedProducts) {
+//             allProducts = storedProducts;
+//             showHTML();
+//         }
+//         await fetchData(); // Llamada a la función para obtener datos
+//     } catch (error) {
+//         console.error('Error during initial setup:', error);
+//     }
+// });
+
+
 const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector('.container-cart-products');
 
@@ -33,8 +184,22 @@ productsList.addEventListener('click', async (e) => {
 
         if (exists !== -1) {
             allProducts[exists].quantity++;
+            Toastify({
+                text: `¡Se agregó un ${infoProduct.title} más al carrito!`,
+                duration: 3000,
+                close: true,
+                gravity: 'bottom',
+                backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+            }).showToast();
         } else {
             allProducts.push(infoProduct);
+            Toastify({
+                text: `¡Se agregó ${infoProduct.title} al carrito!`,
+                duration: 3000,
+                close: true,
+                gravity: 'bottom',
+                backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+            }).showToast();
         }
 
         showHTML();
@@ -116,17 +281,6 @@ const showHTML = () => {
     countProducts.innerText = totalOfProducts;
 };
 
-const fetchData = async () => {
-    try {
-        const response = await fetch('URL_DE_TU_API_O_DATOS');
-        const data = await response.json();
-        // Procesar los datos recibidos si es necesario
-        console.log('Datos obtenidos:', data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-};
-
 const saveProductsLocally = async () => {
     try {
         localStorage.setItem('products', JSON.stringify(allProducts));
@@ -142,7 +296,6 @@ window.addEventListener('load', async () => {
             allProducts = storedProducts;
             showHTML();
         }
-        await fetchData(); // Llamada a la función para obtener datos
     } catch (error) {
         console.error('Error during initial setup:', error);
     }
